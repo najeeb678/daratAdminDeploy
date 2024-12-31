60783079ae09484aacb94e2405e5508cfa59988b
najib <najibqazi777@gmail.com>
2024-12-31 16:44:13 +0500

    baseURL updates

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/",
+  // baseURL: "http://localhost:4000/",
+  baseURL: "http://ec2-15-207-100-201.ap-south-1.compute.amazonaws.com:4000/",
+
   headers: {
     "Content-Type": "application/json",
   },
